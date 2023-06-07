6345fc97383313d7c28881a3ac05438b0461e337
Bhardwaj, Mayuresh (Mayur) <Bhardwaj.Mayuresh@mayo.edu>
2023-06-07 13:11:00 -0400

    chore: adding more changes

--- generate_explanation.py
+++ generate_explanation.py
@@ -1,9 +1,9 @@
-import os
-import sys
 import openai
+import os
 import requests
+import sys
 
-openai.api_key = "YOUR_API_KEY"  # Replace with your OpenAI API key
+openai.api_key = os.environ['OPENAI_API_KEY']  # Replace with your OpenAI API key
 
 def generate_explanation(changes):
     prompt = f"Changes: {changes}\n\nExplain the changes:"
@@ -25,46 +25,6 @@ def generate_explanation(changes):
 pull_request_number = os.environ["PR_NUMBER"]
 repository = os.environ["GITHUB_REPOSITORY"]
 token = os.environ["GH_TOKEN"]
-
-pull_request_url = f"https://api.github.com/repos/{repository}/pulls/{pull_request_number}"
-headers = {
-    "Accept": "application/vnd.github.v3+json",
-    "Authorization": f"Bearer {token}",
-}
-
-response = requests.get(pull_request_url, headers=headers)
-pull_request_data = response.json()
-
-To extract the code changes from a pull request, you can use the GitHub API to fetch the pull request diff. Here's an example of how you can modify the code to extract the code changes:
-
-python
-
-import openai
-import os
-import requests
-
-openai.api_key = "YOUR_API_KEY"  # Replace with your OpenAI API key
-
-def generate_explanation(changes):
-    prompt = f"Changes: {changes}\n\nExplain the changes:"
-
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=100,
-        temperature=0.7,
-        n=1,
-        stop=None,
-        timeout=30,
-    )
-
-    explanation = response.choices[0].text.strip()
-    return explanation
-
-# Get the pull request information from GitHub API
-pull_request_number = os.environ["PR_NUMBER"]
-repository = os.environ["GITHUB_REPOSITORY"]
-token = os.environ["GITHUB_TOKEN"]
 
 pull_request_url = f"https://api.github.com/repos/{repository}/pulls/{pull_request_number}"
 headers = {
