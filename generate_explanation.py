b20460f3024e69e3fa4f5a200d1eb1beee079d11
mayureshbhardwaj <56265677+mayureshbhardwaj@users.noreply.github.com>
2023-06-07 17:25:12 -0400

    Update generate_explanation.py

--- generate_explanation.py
+++ generate_explanation.py
@@ -7,66 +7,66 @@ import json
 openai.api_key = os.environ['OPENAI_API_KEY']  # Replace with your OpenAI API key
 
 def generate_explanation(changes):
-    if not isinstance(changes, list):
-        print("Error: Changes must be a list.")
-        return []
+    prompt = f"Changes: {changes}\n\nExplain the changes:"
 
-    explanations = []
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=100,
+        temperature=0.7,
+        n=1,
+        stop=None,
+        timeout=30,
+    )
 
-    for change in changes:
-        file_name = change.get("filename")
-        patch = change.get("patch")
-
-        if file_name is None or patch is None:
-            continue
-
-        # Extract the code snippet from the change
-        code_lines = []
-        for line in patch.split('\n'):
-            if line.startswith('+') or line.startswith('-'):
-                code_lines.append(line[1:])  # Remove the '+' or '-' character
-        code_snippet = '\n'.join(code_lines)
-
-        # Generate the explanation for the code change using OpenAI ChatGPT
-        prompt = f'Explain the following code change in file "{file_name}":\n\n{code_snippet}'
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=100,
-            temperature=0.7,
-            n=1,
-            stop=None,
-            timeout=30,
-        )
-        explanation = response.choices[0].text.strip()
-        explanations.append((file_name, code_snippet, explanation))
-
-    return explanations
+    explanation = response.choices[0].text.strip()
+    return explanation
 
 # Get the pull request information from GitHub API
 pull_request_number = os.environ["PR_NUMBER"]
 repository = os.environ["GITHUB_REPOSITORY"]
 token = os.environ["GITHUB_TOKEN"]
 
-# Retrieve the files changed in the pull request
+# print the values for troubleshooting
+# print("Pull Request Number:", pull_request_number)
+# print("Repository:", repository)
+# print("Token:", token)
+
+
 pull_request_url = f"https://api.github.com/repos/{repository}/pulls/{pull_request_number}"
 headers = {
     "Accept": "application/vnd.github.v3+json",
     "Authorization": f"Bearer {token}",
 }
+
 response = requests.get(pull_request_url, headers=headers)
 pull_request_data = response.json()
 
-changes = pull_request_data.get("changed_files", [])
+# Retrieve the base and head commits
+base_commit_sha = pull_request_data["base"]["sha"]
+head_commit_sha = pull_request_data["head"]["sha"]
 
-# Generate explanations for the code changes
-explanations = generate_explanation(changes)
+# Get the base and head commit data
+commit_url = f"https://api.github.com/repos/{repository}/commits/"
+base_commit_url = commit_url + base_commit_sha
+head_commit_url = commit_url + head_commit_sha
 
-# Print the code snippets and their corresponding explanations
-for file_name, code_snippet, explanation in explanations:
-    print("File:", file_name)
-    print("Code Snippet:")
-    print(code_snippet)
-    print("\nExplanation:")
-    print(explanation)
-    print()
+response = requests.get(base_commit_url, headers=headers)
+base_commit_data = response.json()
+
+response = requests.get(head_commit_url, headers=headers)
+head_commit_data = response.json()
+
+# Retrieve the files changed between base and head commits
+compare_url = f"https://api.github.com/repos/{repository}/compare/{base_commit_sha}...{head_commit_sha}"
+response = requests.get(compare_url, headers=headers)
+compare_data = response.json()
+
+# Extract the code changes from the compare data
+changes = compare_data["files"]
+
+# Generate explanation using ChatGPT
+explanation = generate_explanation(changes)
+
+# Print or use the generated explanation as needed
+print("\n".join(explanation.split("-")))
